376a41a3ea82f640a942e1e69d1901f856e37d01
Masud Hasan <masudbook@gmail.com>
2017-12-19 23:09:15 +0100

    intial commit

---
+++ pattern-singleton/src/main/java/com/hcl/ingbootcamp/javadesignpattern/singleton/MyThradeSafeSingleton.java
@@ -0,0 +1,37 @@
+package com.hcl.ingbootcamp.javadesignpattern.singleton;
+
+/**
+ * 
+ * @author masud
+ *
+ */
+public class MyThradeSafeSingleton {
+
+	private static MyThradeSafeSingleton instance;;
+
+	private MyThradeSafeSingleton() {
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public synchronized MyThradeSafeSingleton getInstance() {
+		if (instance == null) {
+			instance = new MyThradeSafeSingleton();
+		}
+		return instance;
+	}
+
+	public static MyThradeSafeSingleton getInstanceUsingDoubleLocking() {
+		if (instance == null) {
+			synchronized (MyThradeSafeSingleton.class) {
+				if (instance == null) {
+					instance = new MyThradeSafeSingleton();
+				}
+			}
+		}
+		return instance;
+	}
+
+}
